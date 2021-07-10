c4926e14b46fdfda7e1f0792544abc2b8fb37294
Kristen Schaefer <eightdaw@gmail.com>
2021-07-09 23:41:22 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '342698587504550',
+        clientSecret: 'f42649076f77991c07ae2f24648a8b1b'
+    }
 }
